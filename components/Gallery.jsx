843ceb76a1de179efba17470c283c41c3dd1e0b4
Talha Şahin <sahintalha0@outlook.com>
2022-03-16 10:33:15 +0300

    Update Gallery.jsx

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -13,19 +13,19 @@ export const Gallery = () => {
     <div id="gallery" className="relative py-20 bg-primary">
       <h4 id='theteam'>The Team</h4>
       <Carousel variant="dark" breakPoints={breakPoints}>
-        <a href='https://twitter.com/shintalha' target="_blank">
+        <a href='https://twitter.com/shintalha' rel="noreferrer" target="_blank">
           <div className="item">
             <img width="100%" src="https://i.ibb.co/KzZtNT3/2.jpg" alt="photo" />
             <h1><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/1200px-Twitter-logo.svg.png" alt="twitter"  width="40px"/> @shintalha </h1>
           </div>
         </a>
-        <a href='https://www.instagram.com/serkan.demirs/' target="_blank">
+        <a href='https://www.instagram.com/serkan.demirs/' rel="noreferrer" target="_blank">
           <div className="item">
             <img width="100%" src="https://i.ibb.co/J2wHd1Q/4.jpg" alt="photo" />
             <h1><img src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg" alt="twitter"  width="40px"/> @serkan.demirs </h1>
           </div>
         </a>
-        <a href='https://www.artstation.com/re1mon' target="_blank">
+        <a href='https://www.artstation.com/re1mon' rel="noreferrer" target="_blank">
           <div className="item">
             <img width="100%" src="https://i.ibb.co/6F8K84P/3.jpg" alt="photo" />
             <h1><img src="/artstation.svg" alt="twitter"  width="40px"/> @re1mon </h1>
