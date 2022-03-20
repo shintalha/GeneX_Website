79e84dc52a292912be56ac386a0c0adc76a1e165
Talha Şahin <sahintalha0@outlook.com>
2022-03-20 17:09:23 +0300

    eskişehir-kütahya

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -13,29 +13,29 @@ export const Gallery = () => {
     <div id="gallery" className="relative py-20 bg-primary">
       <h4 id='theteam'>The Team</h4>
       <Carousel variant="dark" breakPoints={breakPoints}>
-        <a href='https://twitter.com/shintalha' rel="noreferrer" target="_blank">
+        <a>
           <div className="item">
             <img width="100%" src="https://i.ibb.co/KzZtNT3/2.jpg" alt="photo" />
-            <h1><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/1200px-Twitter-logo.svg.png" alt="twitter"  width="40px"/> @shintalha </h1>
+            <h1><img src="/iconnew.ico" alt="twitter"  width="40px"/> @leadgene </h1>
           </div>
         </a>
-        <a href='https://www.instagram.com/serkan.demirs/' rel="noreferrer" target="_blank">
+        <a>
           <div className="item">
             <img width="100%" src="https://i.ibb.co/J2wHd1Q/4.jpg" alt="photo" />
-            <h1><img src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg" alt="twitter"  width="40px"/> @serkan.demirs </h1>
+            <h1><img src="/iconnew.ico" alt="twitter"  width="40px"/> zebreyn </h1>
           </div>
         </a>
-        <a href='https://www.artstation.com/re1mon' rel="noreferrer" target="_blank">
+        <a>
           <div className="item">
             <img width="100%" src="https://i.ibb.co/6F8K84P/3.jpg" alt="photo" />
-            <h1><img src="/artstation.svg" alt="twitter"  width="40px"/> @re1mon </h1>
+            <h1><img src="/iconnew.ico" alt="twitter"  width="40px"/> re11mon </h1>
           </div>
         </a>
         
         <a>
           <div className="item">
             <img width="100%" src="https://i.ibb.co/ccCvzdY/image.jpg" alt="photo" />
-            <h1><img src="/iconnew.ico" alt="twitter"  width="40px"/> mismiskrali </h1>
+            <h1><img src="/iconnew.ico" alt="twitter"  width="40px"/> mismisking </h1>
           </div>
         </a>
         
