179ff7fd4b37db8e5b149bf7677c72046638e729
Talha Şahin <sahintalha0@outlook.com>
2022-03-26 18:38:29 +0300

    Update Ptext.jsx

--- components/Ptext.jsx
+++ components/Ptext.jsx
@@ -7,7 +7,7 @@ export const Ptext = () => {
         <div className="about">
 
           <h1>ABOUT THE PROJECT</h1>
-          <h2>The GeneX Project is a unique and rare 3D NFT collection of 10,000 NFTs created by randomly generating over 200 traits. The collection is based on the Ethereum blockchain and includes 10k unique GeneX.</h2>
+          <h2>The GeneX Project is a unique and rare 3D NFT collection of 4444 NFTs created by randomly generating over 200 traits. The collection is based on the Ethereum blockchain and includes 4444 unique GeneX.</h2>
           <ReactTypingEffect
             cursorRenderer={cursor => <h3>{cursor}</h3>}
             text={["Gene X is an arrangement of our genes that determines the characteristics of all of us, transferred to WEB3. We think that this gene will represent our appearance and personality in the Metaverse. Join our family to find your GeneX item that you can best represent yourself on WEB3! "]}
