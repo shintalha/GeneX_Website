38dd76acac08c14177522fab80f2d86197238afe
Talha Şahin <sahintalha0@outlook.com>
2022-04-27 03:22:56 +0300

    publishID

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,3 +1,4 @@
+import { Center } from "@react-three/drei";
 import React from "react"
 import Carousel from "react-elastic-carousel";
 
@@ -16,26 +17,43 @@ export const Gallery = () => {
         <a>
           <div className="item">
             <img width="100%" src="/redox.jpg" alt="photo" />
-            <h1 style={{height: "40px"}}><img src="/iconnew.ico" alt="twitter" width="40px" /> redox </h1>
+            <div className="flex items-center space-x-16" >
+            <a href="https://www.linkedin.com/in/sahintalha/" target="_blank" rel="noreferrer"> <img style={{marginTop: "10px"}} src="/linkedn.png" alt="twitter" width="40px" /> </a>
+            <h1 style={{height: "40px"}}>redox</h1>
+            <a href="https://twitter.com/shintalha" target="_blank" rel="noreferrer"><img style={{marginTop: "10px", marginRight: "2px"}} src="/twitter.png" alt="twitter" width="40px" /> </a>
+            </div>
+            
           </div>
         </a>
         <a>
           <div className="item">
             <img width="100%" src="/mrTreky.jpg" alt="photo" />
-            <h1 style={{height: "40px"}}><img src="/iconnew.ico" alt="twitter"  width="40px"/> mrTreky </h1>
+            <div className="flex items-center space-x-12" >
+            <a href="https://www.linkedin.com/in/cengiz-kilicaslan-53355b135/" target="_blank" rel="noreferrer"><img style={{marginTop: "10px"}} src="/linkedn.png" alt="twitter" width="40px" /> </a>
+            <h1 style={{height: "40px"}}>mrtreky </h1>
+            <a href="https://twitter.com/cengiz_kaslan" target="_blank" rel="noreferrer"><img style={{marginTop: "10px"}} src="/twitter.png" alt="twitter" width="40px" /></a>
+            </div>
           </div>
         </a>
         <a>
           <div className="item">
             <img width="100%" src="/YDalek.jpg" alt="photo" />
-            <h1 style={{height: "40px"}}><img src="/iconnew.ico" alt="twitter"  width="40px"/> YDalek </h1>
+            <div className="flex items-center space-x-16" >
+            <a href="https://www.linkedin.com/in/farukbaktas/" target="_blank" rel="noreferrer"><img style={{marginTop: "10px"}} src="/linkedn.png" alt="twitter" width="40px" /> </a>
+            <h1 style={{height: "40px"}}>ydalek</h1>
+            <a href="https://twitter.com/FarukBaktas" target="_blank" rel="noreferrer"><img style={{marginTop: "10px"}} src="/twitter.png" alt="twitter" width="40px" /></a>
+            </div>
           </div>
         </a>
         
         <a>
           <div className="item">
             <img width="100%" src="/SayeiTmr.jpg" alt="photo" />
-            <h1 style={{height: "40px"}}><img src="/iconnew.ico" alt="twitter"  width="40px"/> SayeiTmr </h1>
+            <div className="flex items-center space-x-12" >
+            <a href="https://www.linkedin.com/in/nihatcuhaci/" target="_blank" rel="noreferrer"><img style={{marginTop: "10px"}} src="/linkedn.png" alt="twitter" width="40px" /> </a>
+            <h1 style={{height: "40px"}}>sayeitmr</h1>
+            <a href="" target="_blank" rel="noreferrer"><img style={{marginTop: "10px"}} src="/twitter.png" alt="twitter" width="40px" /></a>
+            </div>
           </div>
         </a>
 
@@ -50,7 +68,11 @@ export const Gallery = () => {
         <a>
           <div className="item">
             <img width="100%" src="/zebreyn.jpg" alt="photo" />
-            <h1 style={{height: "40px"}}><img src="/iconnew.ico" alt="twitter"  width="40px"/> zebreyn </h1>
+            <div className="flex items-center space-x-14" >
+            <a href="https://www.linkedin.com/in/serkan-demir-6428041b4/" target="_blank" rel="noreferrer"><img style={{marginTop: "10px"}} src="/linkedn.png" alt="twitter" width="40px" /> </a>
+            <h1 style={{height: "40px"}}>zebreyn</h1>
+            <a href="https://www.behance.net/zebrain" target="_blank" rel="noreferrer"><img style={{marginTop: "10px"}} src="/behance.png" alt="twitter" width="40px" /></a>
+            </div>
           </div>
         </a>        
       </Carousel>
